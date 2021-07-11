cee47ec9f8d42756e536424d93c15a273adb0dd6
Fangyi Zhou <me@fangyi.io>
2021-07-11 23:05:40 +0800

    Implement a simple main

--- bib_downloader/__main__.py
+++ bib_downloader/__main__.py
@@ -1,10 +1,20 @@
 import sys
 from typing import List
 
+from . import doi
+
 
 def main(args: List[str]) -> int:
-    print("TODO")
-    return 0
+    if len(args) == 0:
+        input = input("Enter an DOI")
+    else:
+        input = args[0]
+    # TODO: Better input handling
+    try:
+        print(doi.process_input(input))
+        return 0
+    except:
+        return 1
 
 
 if __name__ == "__main__":
